1e548f12e404acab45992ed6c69227043b1f548f
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-09-02 18:15:49 -0300

    início dos exercícios de PHP
    Foram commitados os exercícios 0, 1 e 2

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Olá, mundo \u{1F30E}";
+        ?>
+    </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa porro, mollitia ipsum velit voluptates nihil cumque dolorem nam dolor dolores odio cum amet illo, ad vero vitae, reprehenderit quo ipsam!</p>
+</body>
+</html>
