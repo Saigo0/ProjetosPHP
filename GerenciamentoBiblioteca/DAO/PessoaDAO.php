608c41c77c960247d216e7ced73d079ef8c65e45
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-09-10 14:28:07 -0300

    commit PessoaDAO e UsuarioDAO

--- GerenciamentoBiblioteca/DAO/PessoaDAO.php
+++ GerenciamentoBiblioteca/DAO/PessoaDAO.php
@@ -34,6 +34,38 @@
             }
         }
 
+        public function delete(int $id){
+            $sql = "DELETE FROM pessoa WHERE id = :id";
+            $stmt = $this->pdo->prepare($sql);
+
+            $stmt->execute([':id' => $id]);
+
+            echo "Registro removido";
+        }   
+
+        public function update(Pessoa $pessoa){
+            $sql = "UPDATE Pessoa SET 
+                    nome = :nome,
+                    RG = :RG,
+                    CPF = :CPF,
+                    dataNascimento = :dataNascimento,
+                    email = :email,
+                    endereco = :endereco,
+                    telefone = :telefone WHERE id = :id";
+
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([
+                'id'=> $pessoa->getId(),
+                'nome'=> $pessoa->getNome(),
+                'RG' => $pessoa->getRG(),
+                'CPF' => $pessoa->getCPF(),
+                'dataNascimento' => $pessoa->getDataNascimento(),
+                'email' => $pessoa->getEmail(),
+                'endereco' => $pessoa->getEndereco(),
+                'telefone' => $pessoa->getTelefone()
+            ]);
+        }
+
         
     }
 
--- GerenciamentoBiblioteca/DAO/UsuarioDAO.php
+++ GerenciamentoBiblioteca/DAO/UsuarioDAO.php
@@ -1,5 +1,39 @@
 <?php 
 
+    class UsuarioDAO{
 
+        private PDO $pdo;
+
+        public function __construct(PDO $pdo)
+        {
+            $this->pdo = $pdo;
+        }
+
+        public function create(Usuario $usuario){
+            $sqlPessoa = "INSERT INTO pessoa (nome, RG, CPF, dataNascimento, email, endereco, telefone) VALUES (:nome, :RG, :CPF, :dataNascimento, :email, endereco, telefone)";
+
+            $stmt = $this->pdo->prepare($sqlPessoa);
+            $stmt->execute([
+                'nome' => $usuario->getNome(),
+                'RG' => $usuario->getNome(),
+                'CPF' => $usuario->getCPF(),
+                'dataNascimento' => $usuario->getDataNascimento(),
+                'email' => $usuario->getEmail(),
+                'endereco' => $usuario->getEndereco(),
+                'telefone' => $usuario->getTelefone()
+            ]);
+
+            $idPessoa = $this->pdo->lastInsertId();
+            
+            $sqlUsuario = "INSERT INTO usuario (login, nivelAcesso, senha, dataCadastro) VALUES (:login, :nivelAcesso, :senha, :dataCadastro)";
+
+            $stmt = $this->pdo->prepare($sqlUsuario);
+            $stmt ->execute([
+                
+
+            ]);
+        }
+
+    }
 
 ?>
