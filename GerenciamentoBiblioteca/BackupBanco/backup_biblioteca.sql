35279de177ee3a5aaea5da4351f147b2316864f7
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-10-06 11:42:28 -0300

    update backup banco

--- GerenciamentoBiblioteca/BackupBanco/backup_biblioteca.sql
+++ GerenciamentoBiblioteca/BackupBanco/backup_biblioteca.sql
@@ -28,7 +28,7 @@ CREATE TABLE `administrador` (
   PRIMARY KEY (`id`),
   KEY `fk_usuario_id_admin` (`id_usuario`),
   CONSTRAINT `fk_usuario_id_admin` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,7 @@ CREATE TABLE `administrador` (
 
 LOCK TABLES `administrador` WRITE;
 /*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+INSERT INTO `administrador` VALUES (2,6);
 /*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +56,7 @@ CREATE TABLE `bibliotecario` (
   PRIMARY KEY (`id`),
   KEY `fk_usuario_id_bibl` (`id_usuario`),
   CONSTRAINT `fk_usuario_id_bibl` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,6 +65,7 @@ CREATE TABLE `bibliotecario` (
 
 LOCK TABLES `bibliotecario` WRITE;
 /*!40000 ALTER TABLE `bibliotecario` DISABLE KEYS */;
+INSERT INTO `bibliotecario` VALUES (1,7,'registro','278');
 /*!40000 ALTER TABLE `bibliotecario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +86,7 @@ CREATE TABLE `emprestimo` (
   PRIMARY KEY (`id`),
   KEY `fk_leitor_id` (`id_leitor`),
   CONSTRAINT `fk_leitor_id` FOREIGN KEY (`id_leitor`) REFERENCES `leitor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,6 +95,7 @@ CREATE TABLE `emprestimo` (
 
 LOCK TABLES `emprestimo` WRITE;
 /*!40000 ALTER TABLE `emprestimo` DISABLE KEYS */;
+INSERT INTO `emprestimo` VALUES (3,4,'2025-10-06','2025-10-13','Concluído',''),(4,4,'2025-10-06','2025-10-13','Concluído','');
 /*!40000 ALTER TABLE `emprestimo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,7 +114,7 @@ CREATE TABLE `exemplar` (
   PRIMARY KEY (`id`),
   KEY `id_livro` (`id_livro`),
   CONSTRAINT `id_livro` FOREIGN KEY (`id_livro`) REFERENCES `livro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,7 +123,37 @@ CREATE TABLE `exemplar` (
 
 LOCK TABLES `exemplar` WRITE;
 /*!40000 ALTER TABLE `exemplar` DISABLE KEYS */;
+INSERT INTO `exemplar` VALUES (1,1,'cod1','DISPONIVEL'),(3,1,'cod1','DISPONIVEL');
 /*!40000 ALTER TABLE `exemplar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `itememprestimo`
+--
+
+DROP TABLE IF EXISTS `itememprestimo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itememprestimo` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id_emprestimo` int unsigned NOT NULL,
+  `id_exemplar` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_emprestimo` (`id_emprestimo`),
+  KEY `id_exemplar` (`id_exemplar`),
+  CONSTRAINT `itememprestimo_ibfk_1` FOREIGN KEY (`id_emprestimo`) REFERENCES `emprestimo` (`id`),
+  CONSTRAINT `itememprestimo_ibfk_2` FOREIGN KEY (`id_exemplar`) REFERENCES `exemplar` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itememprestimo`
+--
+
+LOCK TABLES `itememprestimo` WRITE;
+/*!40000 ALTER TABLE `itememprestimo` DISABLE KEYS */;
+INSERT INTO `itememprestimo` VALUES (2,4,1),(3,4,1);
+/*!40000 ALTER TABLE `itememprestimo` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -137,7 +170,7 @@ CREATE TABLE `leitor` (
   PRIMARY KEY (`id`),
   KEY `fk_usuario_id` (`id_usuario`),
   CONSTRAINT `fk_usuario_id` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -146,7 +179,7 @@ CREATE TABLE `leitor` (
 
 LOCK TABLES `leitor` WRITE;
 /*!40000 ALTER TABLE `leitor` DISABLE KEYS */;
-INSERT INTO `leitor` VALUES (2,3,0);
+INSERT INTO `leitor` VALUES (4,8,0);
 /*!40000 ALTER TABLE `leitor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,7 +200,7 @@ CREATE TABLE `livro` (
   `numPaginas` int NOT NULL,
   `localEdicao` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -176,6 +209,7 @@ CREATE TABLE `livro` (
 
 LOCK TABLES `livro` WRITE;
 /*!40000 ALTER TABLE `livro` DISABLE KEYS */;
+INSERT INTO `livro` VALUES (1,'98889098','O livro 2','Autor','Editora',2025,200,'Aqui');
 /*!40000 ALTER TABLE `livro` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -196,7 +230,7 @@ CREATE TABLE `pessoa` (
   `endereco` varchar(150) NOT NULL,
   `telefone` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -205,7 +239,7 @@ CREATE TABLE `pessoa` (
 
 LOCK TABLES `pessoa` WRITE;
 /*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
-INSERT INTO `pessoa` VALUES (4,'José Pires','89089098','89078956745','2025-09-04','jose.otavio2707@gmail.com','Rua Dr. Getúlio Vargas, N 2109','61983047228');
+INSERT INTO `pessoa` VALUES (7,'José','890098089','08744978934','2025-10-04','sdaosido@gmail.com','rararasd','78978786767'),(8,'Gerso','789789789','89089089078','2009-08-27','ger@gmail.com','Rua E','9089897887'),(9,'leitor2','890890890','87989778978','2019-09-17','leitor@gmail.com','Rua E','90978789809');
 /*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -226,7 +260,7 @@ CREATE TABLE `usuario` (
   PRIMARY KEY (`id`),
   KEY `fk_pessoaid` (`id_pessoa`),
   CONSTRAINT `fk_pessoaid` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -235,7 +269,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (3,4,'lewham','Leitor','$2y$10$nYEnEVvhWHNGq7SrmatCs.eQt/jXZe0AwZV7GAx6oPXXMv/uJmclC','2025-09-27');
+INSERT INTO `usuario` VALUES (6,7,'jos123','ADMINISTRADOR','$2y$10$QfycmTP15MnO.dgmiJ09mO1pB7zsQuzFqAoAROS/nZm0hL24gzwIu','2025-10-04'),(7,8,'ger123','BIBLIOTECARIO','$2y$10$c.A2gVgSkpGaQ2FUGpJFiOYq2xxDOQ8Cvf6EwRcqd3E3iBT0mHZsy','2025-10-04'),(8,9,'lei123','Leitor','$2y$10$tP5gfd.ZdKGha6qhKS.uJOKtRgPG3w5ybhjpVRpNWJh0K5CdSvzxu','2025-10-04');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -248,4 +282,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-27 22:14:23
+-- Dump completed on 2025-10-06 11:39:24
