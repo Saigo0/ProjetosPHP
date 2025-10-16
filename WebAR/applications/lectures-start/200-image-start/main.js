f1ae90f21a5f2295d4b6380adbfed1a572adc2c5
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-10-16 17:28:58 -0300

    alterações 200-image-start

--- WebAR/applications/lectures-start/200-image-start/main.js
+++ WebAR/applications/lectures-start/200-image-start/main.js
@@ -2,9 +2,16 @@ const THREE = window.MediaElementAudioSourceNode.IMAGE;
 
 document.addEventListener('DOMContentLoaded', () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-        container: document.body
+        container: document.body,
+        imageTargetSrc: '../../assets/targets/course-banner.mind',
+
     });
+
     const {renderer, scene, camera} = mindarThree;
 
-    
+    const geometry = new THREE.PlaneGeometry(1,1);
+    const material = new THREE.MeshBasicMaterial({color: 0x0000ff, transparent: true, opacity: 0.5});
+    const plane = new THREE.Mesh(geometry, material);
+
+    const anchor = mindarThree.addAnchor(0);
 });
