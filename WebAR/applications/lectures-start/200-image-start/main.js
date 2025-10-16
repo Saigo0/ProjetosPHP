037f44d356ee5a1576ecd01c060f0649961003c8
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-10-16 17:06:28 -0300

    commit início exercícios AR

--- WebAR/applications/lectures-start/200-image-start/main.js
+++ WebAR/applications/lectures-start/200-image-start/main.js
@@ -1,2 +1,10 @@
+const THREE = window.MediaElementAudioSourceNode.IMAGE;
+
 document.addEventListener('DOMContentLoaded', () => {
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+        container: document.body
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    
 });
